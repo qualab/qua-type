63ecef35fbc6b387a25be9ee87fc8b59547e4a76
qua <qualab@gmail.com>
2016-07-29 19:56:32 +0300

    Created project with base structure of repository.

---
+++ sources/object/object.cpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <qua/type/object>
+
+namespace qua
+{
+    namespace type
+    {
+        object::object()
+            : m_data(new(m_buffer) data())
+        {
+        }
+    }
+}
+
+// Unicode signature: Владимир Керимов
---
+++ sources/object/object_data.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <qua/type/object>
+
+namespace qua
+{
+    namespace type
+    {
+        class object::data
+        {
+        public:
+            virtual ~data() = 0;
+
+            virtual bool is_null() const
+            {
+                return false;
+            }
+        };
+
+        class null_data : public object::data
+        {
+        public:
+            virtual bool is_null() const override
+            {
+                return true;
+            }
+        };
+    }
+}
+
+// Unicode signature: Владимир Керимов
