b378ac5a7790b7effa4aba1ce65684258d6a0001
qua <qualab@gmail.com>
2016-07-29 20:59:41 +0300

    Compilable version with some logic added to object::data.

--- sources/object/object.cpp
+++ sources/object/object.cpp
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include "object_data.hpp"
+#include <qua/type/object>
 
 namespace qua
 {
@@ -15,6 +15,20 @@ namespace qua
         {
             if (m_data)
                 m_data->~data();
+        }
+
+        object::object(const object& another)
+            : m_data()
+        {
+            if (another.m_data)
+                m_data = another.m_data->copy(m_buffer);
+        }
+
+        object::object(object&& temporary)
+            : m_data()
+        {
+            if (temporary.m_data)
+                m_data = temporary.m_data->move(m_buffer);
         }
     }
 }
