66dd2f78c23f091d787cec76b837abc540d3d049
qua <qualab@gmail.com>
2016-07-31 00:09:40 +0300

    Fixed architectural mistakes.

--- sources/object/object.cpp
+++ sources/object/object.cpp
@@ -29,177 +29,43 @@ namespace qua
         object::object(const object& another)
             : _data()
         {
-            if (another._data)
-                _data = another._data->copy(_buffer);
+            *this = another;
         }
 
         object::object(object&& temporary)
             : _data()
         {
-            if (temporary._data)
-                _data = temporary._data->move(_buffer);
+            *this = std::move(temporary);
         }
 
         object& object::operator = (const object& another)
         {
             clear();
-            _data = another._data->copy(_buffer);
+            if (another._data)
+                _data = another._data->copy(_buffer);
             return *this;
         }
 
         object& object::operator = (object&& temporary)
         {
             clear();
-            _data = temporary._data->move(_buffer);
+            if (temporary._data)
+                _data = temporary._data->move(_buffer);
             return *this;
         }
 
         object::object(object::data* derived)
             : _data(derived)
         {
+            if (static_cast<void*>(_data) != buffer())
+            {
+                // TODO: throw exception
+            }
         }
 
         void* object::buffer()
         {
             return static_cast<void*>(_buffer);
-        }
-
-        template <>
-        bool object::get() const
-        {
-        }
-
-        template <>
-        std::int8_t object::get() const
-        {
-        }
-
-        template <>
-        std::int16_t object::get() const
-        {
-        }
-
-        template <>
-        std::int32_t object::get() const
-        {
-        }
-
-        template <>
-        std::int64_t object::get() const
-        {
-        }
-
-        template <>
-        std::uint8_t object::get() const
-        {
-        }
-
-        template <>
-        std::uint16_t object::get() const
-        {
-        }
-
-        template <>
-        std::uint32_t object::get() const
-        {
-        }
-
-        template <>
-        std::uint64_t object::get() const
-        {
-        }
-
-        template <>
-        float object::get() const
-        {
-        }
-
-        template <>
-        double object::get() const
-        {
-        }
-
-        template <>
-        void object::set(bool value)
-        {
-            clear();
-            _data = new(buffer()) scalar<bool>::data(value);
-        }
-
-        template <>
-        void object::set(std::int8_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::int8_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::int16_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::int16_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::int32_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::int32_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::int64_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::int64_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::uint8_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::uint8_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::uint16_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::uint16_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::uint32_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::uint32_t>::data(value);
-        }
-
-        template <>
-        void object::set(std::uint64_t value)
-        {
-            clear();
-            _data = new(_buffer) scalar<std::uint64_t>::data(value);
-        }
-
-        template <>
-        void object::set(float value)
-        {
-            clear();
-            _data = new(_buffer) scalar<float>::data(value);
-        }
-
-        template <>
-        void object::set(double value)
-        {
-            clear();
-            _data = new(_buffer) scalar<double>::data(value);
-        }
-
-        template <>
-        void object::set(std::nullptr_t)
-        {
-            clear();
         }
     }
 }
